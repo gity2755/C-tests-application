c9b6fde185b3b091e0e36dd571429ec4ced09972
Gitty <gity2755@gmail.com>
2024-06-09 19:09:35 +0300

    commit

---
+++ TESTBL/Testbl.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTBL
+{
+    public class Testbl
+    {
+        public Users LogIn(string email,string pass)
+        {
+            Users user;
+            try
+            {
+                using (TestsDBEntities DB = new TestsDBEntities())
+                {
+                   user=DB.Users.FirstOrDefault(x => x.Email == email && x.Pass == pass);
+                    
+                }
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+            return user;
+        }
+      
+        public List<Answers> AnswerForOneQuestion(int questionID)
+        {
+            List<Answers> answers;
+            try
+            {
+                using (TestsDBEntities DB = new TestsDBEntities())
+                {
+                    answers = DB.Answers.Where(x => x.QuestionID == questionID).OrderBy(x => Guid.NewGuid()).ToList();
+
+                }
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+            return answers;
+        }
+        
+         public List<Questions> StartTest()
+        {
+            
+            List<Questions> questions1;
+            List<Questions> questions2;
+            try
+            {
+                using (TestsDBEntities DB = new TestsDBEntities())
+                {
+
+                    questions1 = DB.Questions.Where(x => x.Question_Points == 10).OrderBy(x=>Guid.NewGuid()).Take(8).ToList();
+                    questions2 = DB.Questions.Where(x => x.Question_Points == 5).OrderBy(x => Guid.NewGuid()).Take(4).ToList();
+                    questions1.AddRange(questions2);
+                }
+
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+            return questions1;
+        }
+
+       public void InsertToHistoryTable(Users user,int mark)
+        {
+            try
+            {
+                using (TestsDBEntities DB = new TestsDBEntities())
+                {
+                    MarksHistory history = new MarksHistory();
+                    history.UserID = user.UserID;
+                    history.Mark = mark;
+                    history.TestDate = DateTime.Now;
+                    
+                    DB.MarksHistory.Add(history);
+                    DB.SaveChanges();
+                   
+                }
+            }
+            catch (Exception err)
+            {
+
+                throw err;
+            }
+        }
+    }
+}
---
+++ TESTUI/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TESTBL;
+
+namespace TESTUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Testbl bl = new Testbl();
+            Users user=new Users();
+            Console.WriteLine("Enter email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter a password");
+            string pass = Console.ReadLine();
+            try { 
+            user = bl.LogIn(email, pass);
+            while (user==null)
+            {
+                Console.WriteLine(user);
+                Console.WriteLine("Enter email");
+                 email = Console.ReadLine();
+                Console.WriteLine("Enter a password");
+                 pass = Console.ReadLine();
+                user = bl.LogIn(email, pass);
+            }
+            Console.WriteLine("Hello To "+user.First_Name+"  "+user.Last_Name );
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            List<Questions> listQ=bl.StartTest();
+            int cnt = 0;
+            int points = 0;
+            foreach (Questions q in listQ)
+            {
+                try
+                {
+                 List<Answers> answer = bl.AnswerForOneQuestion(q.QuestionID);
+                
+                
+               
+                cnt++;
+                Console.WriteLine(cnt + ". " + q.Question_Value);
+                if (answer.Count==1)//if its a regular question
+                {
+                    string userResult = Console.ReadLine();
+                    if (userResult == answer.First().Answer_Value)
+                    {
+                        Console.WriteLine("good answer!");
+                      points += Convert.ToInt32(q.Question_Points); 
+
+                    }
+                }
+                else
+                {
+                    int correctAnswer = Convert.ToInt32(q.AnswerID);
+                    int correctIndex = 0;
+                    for (int i =0; i < answer.Count; i++)
+                    {
+                        if (answer[i].AnswerID == correctAnswer)
+                        {
+                            correctIndex = i+1;
+                        }
+                        Console.WriteLine((i+1)+") "+answer[i].Answer_Value);
+                    }
+                    string userResult=Console.ReadLine();
+                    if (userResult == (correctIndex.ToString()))
+                    {
+                        Console.WriteLine("good answer!");
+                        points += Convert.ToInt32(q.Question_Points);
+                    }
+                    else
+                    {
+                        Console.WriteLine("bad answer!");
+                    }
+
+                }
+            }catch (Exception ex)
+            {
+           Console.WriteLine(ex.Message);
+            }
+
+                
+            }
+            Console.WriteLine("you finished the test with mark: "+points+"%");
+            bl.InsertToHistoryTable(user, points);
+
+             Console.ReadLine();  
+        }
+        
+    }
+}
